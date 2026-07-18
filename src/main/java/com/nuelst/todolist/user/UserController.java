3df279285d1d004ae179fafcaf10ef501c71ac2a
manuel <manuelds@outlook.pt>
2026-07-18 13:46:00 +0100

    define user module

---
+++ src/main/java/com/nuelst/todolist/user/UserController.java
@@ -0,0 +1,18 @@
+package com.nuelst.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  @PostMapping("")
+  public void create(@RequestBody UserModel data) {
+
+    System.out.println(data.name);
+
+  }
+
+}
