94f9ae70a3758372ac565bdc8592604a0ca2400c
manuel <manuelds@outlook.pt>
2026-07-18 15:39:36 +0100

    feat: hash password

--- src/main/java/com/nuelst/todolist/user/UserController.java
+++ src/main/java/com/nuelst/todolist/user/UserController.java
@@ -3,6 +3,7 @@ package com.nuelst.todolist.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
   @Autowired
   UserRepository userRepository;
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
 
   @PostMapping("")
   public ResponseEntity create(@RequestBody UserModel data) {
@@ -25,6 +29,9 @@ public class UserController {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username already exists!");
 
     }
+
+    var pwHash = this.passwordEncoder.encode(data.getPassword());
+    data.setPassword(pwHash);
     var newUser = this.userRepository.save(data);
     return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
   }
